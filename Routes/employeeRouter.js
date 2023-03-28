ff75f722e133b057d9149dadae198ecb42cecdb2
sarwar-asik <shasik2021@gmail.com>
2023-03-28 20:17:02 +0600

    get findOne

--- Routes/employeeRouter.js
+++ Routes/employeeRouter.js
@@ -6,15 +6,17 @@ const Employee = new mongoose.model("Employee", employeeSchema);
 
 router.post("/employee", async (req, res) => {
   const newEmployee = new Employee(req.body);
-  console.log(newEmployee);
-  // const result = await Employee.create(newEmployee);
-  // if (result) {
-  //   res.send({ success: true });
-  // }
+  // console.log(newEmployee);
+  const result = await Employee.create(newEmployee);
+  if (result) {
+    res.send({ success: true });
+  }
 });
 
-router.get("/users", async (req, res) => {
+router.get("/users/:email", async (req, res) => {
   try {
+    const query = req.params.email;
+    // console.log(query);
     // Employee.find({}, (error, result) => {
     //   if (error) {
     //     res.status(501).send({ error: "Had an error" });
@@ -22,15 +24,19 @@ router.get("/users", async (req, res) => {
     //     res.send(result);
     //   }
     // });
-    Employee.find({}).then((foundItems) => {
-      if(foundItems.length>0){
-        res.send(foundItems)
-      }
-   
-    })
-    .catch(err=>{
-      res.send({error:err.message})
-    })
+    Employee.findOne({ email: query }).then((getUser) => {
+      res.send(getUser);
+    });
+
+    // Employee.find({})
+    //   .then((foundItems) => {
+    //     if (foundItems.length > 0) {
+    //       res.send(foundItems);
+    //     }
+    //   })
+    //   .catch((err) => {
+    //     res.send({ error: err.message });
+    //   });
   } catch (error) {
     const { name, message, stack } = error;
     console.log({ name, message, stack });
