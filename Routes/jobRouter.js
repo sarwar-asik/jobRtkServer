8c8e562ac56cb83c0e945d8a38e570eec5fad91e
sarwar-asik <shasik2021@gmail.com>
2023-04-04 17:38:42 +0600

    findById in job

--- Routes/jobRouter.js
+++ Routes/jobRouter.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const { ObjectId } = require("mongodb");
 const jobSchema = require("../Schemas/jobSchema");
 const Job = new mongoose.model("JOB", jobSchema);
 
 router.post("/addJob", async (req, res) => {
-
-
   try {
     const newJob = new Job(req.body);
-    console.log(newJob, "from body"); 
+    console.log(newJob, "from body");
 
     Job.create(newJob)
       .then((result) => {
@@ -39,8 +38,26 @@ router.get("/all", async (req, res) => {
     const { name, message, stack } = error;
     console.log({ name, message, stack });
 
-    res.status(500).send({ error: message + " From Server", status: 500 });
+    // res.status(500).send({ error: message + " From Server", status: 500 });
     // res.send({ name, message, stack });
+  }
+});
+
+router.get("/:_id", async (req, res) => {
+  try {
+    const id = req.params._id;
+    const query = { ObjectId: new ObjectId(id) };
+    console.log("query by id ..............", query);
+    const getJob = await Job.findById(id).then((jobs) => jobs);
+    if (getJob) {
+      res.status(200).send({ status: true, data: getJob });
+    } else {
+      console.log("not found in DB", query);
+      res.status(404).send({ status: false, error: "Not found data" });
+    }
+  } catch (error) {
+    const { name, message, stack } = error;
+    console.log({ name, message, stack });
   }
 });
 
