5b8ac067b1b19ecfe997658403612ac556e5d1e7
sarwar-asik <shasik2021@gmail.com>
2023-04-18 02:23:14 +0600

    updating

--- system2/controller/job.controller.js
+++ system2/controller/job.controller.js
@@ -5,7 +5,7 @@ const { bulkDeleteJobByID } = require("../services/job2.services");
 exports.getJobs2 = async (req, res, next) => {
   try {
     console.log("query.......");
-    const jobs = await job2.find({}).select("-jobs -overView -v");
+    const jobs = await job2.find({}).select("-jobs -overView ");
     // console.log(jobs);
 
     res.status(200).json({ status: "success", data: jobs });
